90f64cff5d0c51d0bf960af40dcf4e21cbcfd52e
Hugo Antonio Campos López <hugo.antonio.campos@gmail.com>
2026-09-01 22:15:36 -0600

    redo: correción url.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Importación de Páginas (las crearemos en blanco por ahora)
 import Home from './pages/Home';
 import Estadisticas from './pages/Estadisticas';
 import Equipos from './pages/Equipos';
 import PerfilJugador from './pages/PerfilJugador';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAz6pLf6wMDxsP_TOYq9MISWQyavzBs4vo",
-  authDomain: "futbol-santa-barbara.firebaseapp.com",
-  projectId: "futbol-santa-barbara",
-  storageBucket: "futbol-santa-barbara.firebasestorage.app",
-  messagingSenderId: "345592854227",
-  appId: "1:345592854227:web:2090e70fe231fabbd78626"
-};
-
-// Initialize Firebase
-// Componentes globales
 import Navbar from './components/Navbar';
 import Historial from './components/Historial';
 import { AuthProvider } from './context/AuthContext';
@@ -31,24 +13,23 @@ import { AuthProvider } from './context/AuthContext';
 function App() {
   return (
     <AuthProvider>
-
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        {/* Rutas Públicas */}
-        <Route path="/" element={<Home />} />
-        <Route path="/estadisticas" element={<Estadisticas />} />
-        <Route path="/equipos" element={<Equipos />} />
-        <Route path="/jugadores/:id" element={<PerfilJugador />} />
-        <Route path="/historial" element={<Historial />} />
-        {/* Rutas de Administración */}
-        <Route path="/admin" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-      </Routes>
-    </BrowserRouter>
+      <Router>
+        <Navbar />
+        <Routes>
+          {/* Rutas Públicas */}
+          <Route path="/" element={<Home />} />
+          <Route path="/estadisticas" element={<Estadisticas />} />
+          <Route path="/equipos" element={<Equipos />} />
+          <Route path="/jugadores/:id" element={<PerfilJugador />} />
+          <Route path="/historial" element={<Historial />} />
+          
+          {/* Rutas de Administración */}
+          <Route path="/admin" element={<AdminLogin />} />
+          <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        </Routes>
+      </Router>
     </AuthProvider>
   );
 }
-const app = initializeApp(firebaseConfig);
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/futbol_santa_barbara/', // 👈 Pon aquí el nombre exacto de tu repositorio en GitHub
+  base: '/futbol_santa_barbara/', 
 })
